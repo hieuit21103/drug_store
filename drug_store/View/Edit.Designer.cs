5302f27432ff4ccd7de3fc9ef44300a159f33eb2
Haruna <hieuit21103@gmail.com>
2023-05-03 21:32:59 +0700

    Update

--- drug_store/View/Edit.Designer.cs
+++ drug_store/View/Edit.Designer.cs
@@ -47,7 +47,6 @@
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.ibtnThemf3 = new FontAwesome.Sharp.IconButton();
             this.ibtnTailai3 = new FontAwesome.Sharp.IconButton();
-            this.ibtnsearchf4 = new FontAwesome.Sharp.IconButton();
             this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
@@ -57,31 +56,32 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.label1.Location = new System.Drawing.Point(31, 20);
+            this.label1.Location = new System.Drawing.Point(41, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(233, 31);
+            this.label1.Size = new System.Drawing.Size(289, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Cập Nhập Thuốc";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.label2.Location = new System.Drawing.Point(76, 113);
+            this.label2.Location = new System.Drawing.Point(117, 193);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.Size = new System.Drawing.Size(55, 16);
             this.label2.TabIndex = 0;
             this.label2.Text = "ID thuốc";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.label3.Location = new System.Drawing.Point(85, 236);
+            this.label3.Location = new System.Drawing.Point(117, 302);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.Size = new System.Drawing.Size(72, 16);
             this.label3.TabIndex = 0;
             this.label3.Text = "Tên Thuốc";
             // 
@@ -89,9 +89,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.label4.Location = new System.Drawing.Point(85, 312);
+            this.label4.Location = new System.Drawing.Point(114, 400);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.Size = new System.Drawing.Size(59, 16);
             this.label4.TabIndex = 0;
             this.label4.Text = "Số thuốc";
             // 
@@ -99,9 +100,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.label5.Location = new System.Drawing.Point(85, 393);
+            this.label5.Location = new System.Drawing.Point(114, 494);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.Size = new System.Drawing.Size(92, 16);
             this.label5.TabIndex = 0;
             this.label5.Text = "Ngày sản xuất";
             // 
@@ -109,9 +111,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.label6.Location = new System.Drawing.Point(588, 98);
+            this.label6.Location = new System.Drawing.Point(785, 146);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.Size = new System.Drawing.Size(86, 16);
             this.label6.TabIndex = 0;
             this.label6.Text = "Ngày hết hạn";
             // 
@@ -119,9 +122,10 @@
             // 
             this.label7.AutoSize = true;
             this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.label7.Location = new System.Drawing.Point(595, 311);
+            this.label7.Location = new System.Drawing.Point(785, 400);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(96, 13);
+            this.label7.Size = new System.Drawing.Size(117, 16);
             this.label7.TabIndex = 0;
             this.label7.Text = "Giá cho mỗi đơn vị";
             // 
@@ -129,78 +133,88 @@
             // 
             this.label8.AutoSize = true;
             this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.label8.Location = new System.Drawing.Point(598, 178);
+            this.label8.Location = new System.Drawing.Point(785, 230);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(84, 13);
+            this.label8.Size = new System.Drawing.Size(103, 16);
             this.label8.TabIndex = 0;
             this.label8.Text = "Số lượng có sẵn";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(79, 140);
+            this.textBox1.Location = new System.Drawing.Point(117, 213);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.Size = new System.Drawing.Size(265, 22);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(88, 262);
+            this.textBox2.Location = new System.Drawing.Point(117, 322);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(200, 20);
+            this.textBox2.Size = new System.Drawing.Size(265, 22);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(88, 341);
+            this.textBox3.Location = new System.Drawing.Point(117, 420);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(200, 20);
+            this.textBox3.Size = new System.Drawing.Size(265, 22);
             this.textBox3.TabIndex = 1;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(591, 203);
+            this.textBox4.Location = new System.Drawing.Point(788, 250);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(200, 20);
+            this.textBox4.Size = new System.Drawing.Size(265, 22);
             this.textBox4.TabIndex = 1;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(591, 341);
+            this.textBox5.Location = new System.Drawing.Point(788, 420);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(200, 20);
+            this.textBox5.Size = new System.Drawing.Size(265, 22);
             this.textBox5.TabIndex = 1;
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(88, 421);
+            this.dateTimePicker1.Location = new System.Drawing.Point(117, 518);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
             this.dateTimePicker1.TabIndex = 2;
             // 
             // dateTimePicker2
             // 
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker2.Location = new System.Drawing.Point(591, 135);
+            this.dateTimePicker2.Location = new System.Drawing.Point(788, 166);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.Size = new System.Drawing.Size(265, 22);
             this.dateTimePicker2.TabIndex = 2;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.label9.Location = new System.Drawing.Point(703, 263);
+            this.label9.Location = new System.Drawing.Point(937, 324);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(77, 13);
+            this.label9.Size = new System.Drawing.Size(96, 16);
             this.label9.TabIndex = 0;
             this.label9.Text = "Thêm số lượng";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(786, 261);
+            this.numericUpDown1.Location = new System.Drawing.Point(1048, 321);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(61, 22);
             this.numericUpDown1.TabIndex = 3;
             // 
             // ibtnThemf3
@@ -214,14 +228,14 @@
             this.ibtnThemf3.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.ibtnThemf3.IconSize = 30;
             this.ibtnThemf3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ibtnThemf3.Location = new System.Drawing.Point(591, 388);
+            this.ibtnThemf3.Location = new System.Drawing.Point(788, 478);
+            this.ibtnThemf3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ibtnThemf3.Name = "ibtnThemf3";
-            this.ibtnThemf3.Size = new System.Drawing.Size(100, 40);
+            this.ibtnThemf3.Size = new System.Drawing.Size(133, 49);
             this.ibtnThemf3.TabIndex = 4;
             this.ibtnThemf3.Text = "Thêm";
             this.ibtnThemf3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.ibtnThemf3.UseVisualStyleBackColor = false;
-            this.ibtnThemf3.Click += new System.EventHandler(this.ibtnThemf3_Click);
             // 
             // ibtnTailai3
             // 
@@ -234,47 +248,30 @@
             this.ibtnTailai3.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.ibtnTailai3.IconSize = 30;
             this.ibtnTailai3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ibtnTailai3.Location = new System.Drawing.Point(742, 388);
+            this.ibtnTailai3.Location = new System.Drawing.Point(989, 478);
+            this.ibtnTailai3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ibtnTailai3.Name = "ibtnTailai3";
-            this.ibtnTailai3.Size = new System.Drawing.Size(100, 40);
+            this.ibtnTailai3.Size = new System.Drawing.Size(133, 49);
             this.ibtnTailai3.TabIndex = 5;
             this.ibtnTailai3.Text = "Tải Lại";
             this.ibtnTailai3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.ibtnTailai3.UseVisualStyleBackColor = false;
             // 
-            // ibtnsearchf4
-            // 
-            this.ibtnsearchf4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.ibtnsearchf4.FlatAppearance.BorderSize = 0;
-            this.ibtnsearchf4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ibtnsearchf4.ForeColor = System.Drawing.Color.White;
-            this.ibtnsearchf4.IconChar = FontAwesome.Sharp.IconChar.MagnifyingGlass;
-            this.ibtnsearchf4.IconColor = System.Drawing.Color.LightBlue;
-            this.ibtnsearchf4.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.ibtnsearchf4.IconSize = 20;
-            this.ibtnsearchf4.Location = new System.Drawing.Point(184, 171);
-            this.ibtnsearchf4.Name = "ibtnsearchf4";
-            this.ibtnsearchf4.Size = new System.Drawing.Size(95, 25);
-            this.ibtnsearchf4.TabIndex = 6;
-            this.ibtnsearchf4.Text = "Search";
-            this.ibtnsearchf4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.ibtnsearchf4.UseVisualStyleBackColor = false;
-            // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.panel1.Location = new System.Drawing.Point(436, 68);
+            this.panel1.Location = new System.Drawing.Point(581, 84);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(6, 338);
+            this.panel1.Size = new System.Drawing.Size(8, 416);
             this.panel1.TabIndex = 7;
             // 
-            // frmModifiMedicine
+            // Edit
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(924, 486);
+            this.ClientSize = new System.Drawing.Size(1232, 598);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.ibtnsearchf4);
             this.Controls.Add(this.ibtnTailai3);
             this.Controls.Add(this.ibtnThemf3);
             this.Controls.Add(this.numericUpDown1);
@@ -294,7 +291,8 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label1);
-            this.Name = "frmModifiMedicine";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Edit";
             this.Text = "frmModifiMedicine";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
@@ -323,6 +321,5 @@
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private FontAwesome.Sharp.IconButton ibtnThemf3;
         private FontAwesome.Sharp.IconButton ibtnTailai3;
-        private FontAwesome.Sharp.IconButton ibtnsearchf4;
         private System.Windows.Forms.Panel panel1;
     }
---
+++ drug_store/View/ValidCheck.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace drug_store.View
+{
+    partial class ValidCheck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(575, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kiểm Tra Hạn Sử Dụng Của Thuốc";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 202);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(985, 358);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(767, 122);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Phân loại";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(772, 151);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(257, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // ValidCheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 668);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ValidCheck";
+            this.Text = "frmMedicineVlyCheck";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
